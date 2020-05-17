549ec42ec47f3fd3429ad3fb71aa8856619751e9
antondroid <win22n@yandex.ru>
2020-05-17 17:12:51 +0300

    reverse addd

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -1,18 +1,17 @@
 package ru.progwards.java1.lessons.basics;
 
 public class ReverseDigits {
-    public static int reverseDigits(int number){
-        int n = 12345678;
-        System.out.println(n);
-
+    public static int reverseDigits(int number) {
         int n1 = 0;
-        while(n != 0){
-            n1 = n1*10 + n%10;
-            n /= 10;
+        while (number != 0) {
+            n1 = n1 * 10 + number % 10;  //528%10==8
+            number /= 10; // 3/10==0
         }
-        System.out.println(n1);
         return (n1);
+    }
 
+    public static void main(String[] args) {
+        System.out.println("reverse = " + reverseDigits(38625));
     }
 
 }
